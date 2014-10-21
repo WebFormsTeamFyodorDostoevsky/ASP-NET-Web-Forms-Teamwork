ed7ffd85409a3cd15ddd261c09cdf0f401aadb7b
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-10-21 18:19:04 +0300

    create barter offer TODO: success message

--- BarterSystem/BarterSystem.Models/Advertisment.cs
+++ BarterSystem/BarterSystem.Models/Advertisment.cs
@@ -13,7 +13,7 @@
         [Required]
         public string Content { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
 
         public User User { get; set; }
 
